98c358f2e3b9d05a2f015bbae0382a7e8b77c5bc
Sun <@purdue.edu>
2015-10-08 11:13:58 -0400

    change

--- calc.py
+++ calc.py
@@ -33,8 +33,14 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+#Mod function
+#a -- dividend
+#b -- divisor
+def mod(a,b):
+    return a % b
 
 # -------------------------------------------------------- #
+
 
 
 # -------------------------------------------------------- #
@@ -67,6 +73,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+        elif (op == "%"):
+            print "Mod: ", mod(a, b)
         else:
             print "Invalid operation..."
 
@@ -75,3 +83,4 @@ while (True):
         break
 
 # -------------------------------------------------------- #
+
